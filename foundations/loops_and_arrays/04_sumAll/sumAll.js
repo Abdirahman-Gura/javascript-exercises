46fdaece6255e551a078e071e07d9c503ddd75e1
Abdirahman Gura <manowgura@gmail.com>
2026-09-21 10:15:29 +0300

    Complete sumAll exercise

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (!Number.isInteger(a) || a <= 0 || !Number.isInteger(b) || b <= 0) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
