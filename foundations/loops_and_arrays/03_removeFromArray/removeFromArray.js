04c1b7b35eecf74c104c420b0029f2e903ea7696
Abdirahman Gura <manowgura@gmail.com>
2026-09-21 09:21:57 +0300

    Complete removeFromArray exercise

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  const result = array.filter(function(item) {
+    return !args.includes(item);
+  });
+
+  return result;
 };
 
 // Do not edit below this line
