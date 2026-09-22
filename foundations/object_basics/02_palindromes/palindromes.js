5640a446a3d6c3882360985ca315650b317d41c7
Abdirahman Gura <manowgura@gmail.com>
2026-09-22 15:08:33 +0300

    Complete palindromes exercise

--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function(string) {
+  const lower = string.toLowerCase();
+  const clean = lower.replace(/[^a-z0-9]/g, "");
+  const reversed = clean.split("").reverse().join("");
 
+  return clean === reversed;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
