a213c4b2eda28654a33d46d9090ea835008a45ad
Abdirahman Gura <manowgura@gmail.com>
2026-09-23 09:02:09 +0300

    Complete fibonacci exercise

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+ number = Number(number);
+    if (number < 0) {
+    return "OOPS";
+  }
+  if (number === 0) {
+  return 0;
+}
 
+  if (number === 1 || number === 2) {
+    return 1;
+  }
+
+  let previous = 1;
+  let current = 1;
+
+  for (let i = 3; i <= number; i++) {
+    let next = previous + current;
+
+    previous = current;
+    current = next;
+  }
+
+  return current;
 };
 
 // Do not edit below this line
